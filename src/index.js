25284c85501155bedfe458c3ba0a9e1a1e6d7989
vishnuharish <vishnuharish12@gmail.com>
2020-01-24 14:34:50 +0530

    Component Architecture

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './components/Main';
-
+import './styles/stylesheets.css';
 ReactDOM.render(<Main />, document.getElementById('root'));
