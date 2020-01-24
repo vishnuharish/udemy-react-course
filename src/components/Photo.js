9a76042d20a97c69cd435faab2b848f779554a41
vishnuharish <vishnuharish12@gmail.com>
2020-01-24 17:48:34 +0530

    with lifeCycle method didMount

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,16 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class Photo extends Component {
-    render(){
-        const post = this.props.post;
-        return <figure className="figure"> 
-            <img className="photo" src={post.imageLink} alt={post.description}/>
-                <figcaption><p> {post.description} </p></figcaption>
-            <div className="button-container">
-                <button className="remove-button"> Remove </button>
-            </div>
-        </figure>
-    }
+
+function Photo(props){
+    const post = props.post;
+    return <figure className="figure"> 
+             <img className="photo" src={post.imageLink} alt={post.description}/>
+                 <figcaption><p> {post.description} </p></figcaption>
+             <div className="button-container">
+                 <button className="remove-button" onClick={ () => {
+                     props.onRemovePhoto(post)
+                     }}> Remove </button>
+             </div>
+         </figure>
 }
+
+Photo.propTypes = {
+    post: PropTypes.object.isRequired,
+    onRemovePhoto: PropTypes.func.isRequired
+}
+
 
 export default Photo;
--- src/components/PhotoWall.js
+++ src/components/PhotoWall.js
@@ -1,11 +1,17 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Photo from './Photo';
-class PhotoWall extends Component {
-    render(){
-        return <div className="photoGrid">
-                {this.props.posts.map((post, index) => <Photo key = {index} post={post}/>)}
-        </div>
-    }
+import PropTypes from 'prop-types';
+
+function PhotoWall(props){
+    return <div className="photoGrid">
+                {props.posts.map((post, index) => <Photo key = {index} post={post} onRemovePhoto={props.onRemovePhoto}/>)}
+            </div>
 }
+
+PhotoWall.propTypes = {
+    posts: PropTypes.array.isRequired,
+    onRemovePhoto: PropTypes.func.isRequired
+}
+
 
 export default PhotoWall;
